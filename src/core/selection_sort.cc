f9ef2fea31c770c503523e2bc85759a50ac34f3e
T_RIK_E <rterada123@gmail.com>
2020-11-24 23:38:20 -0600

    Completed selection sort

--- src/core/selection_sort.cc
+++ src/core/selection_sort.cc
@@ -10,24 +10,30 @@ namespace sorting {
 SelectionSort::SelectionSort(std::vector<size_t> unsorted_list)
     : Sort(std::move(unsorted_list)) {
   i_ = 0;
+  min_idx_ = i_;
   j_ = i_ + 1;
 }
 
 void SelectionSort::SortOneStep() {
   // One by one move boundary of unsorted subarray
-  if (i_ < unsorted_list_.size() - 1) {
+  if (i_ < static_cast<int>(unsorted_list_.size()) - 1) {
+    height1_ = unsorted_list_[min_idx_];
     // Find the minimum element in unsorted array
-    min_idx_ = i_;
-    if (j_ < unsorted_list_.size()) {
+    if (j_ < static_cast<int>(unsorted_list_.size())) {
+      height2_ = unsorted_list_[j_];
       if (unsorted_list_[j_] < unsorted_list_[min_idx_]) {
         min_idx_ = j_;
       }
-    }
+      j_++;
+    } else {
+      int temp = unsorted_list_[i_];
+      unsorted_list_[i_] = unsorted_list_[min_idx_];
+      unsorted_list_[min_idx_] = temp;
 
-    /**
-    // Swap the found minimum element with the first element
-    swap(&arr[min_idx_], &arr[i_]);
-     **/
+      i_++;
+      min_idx_ = i_;
+      j_ = i_ + 1;
+    }
   }
 }
 }  // namespace sorting
